9588c51c5583f5d903a4586d9be8e92ec08656cc
Shuchita Gupta <shuchi2133@gmail.com>
2024-12-13 11:22:02 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration System which ensures all validations are in place during a User Entry.");
+    }
+}
