2d12ff6967fe620fc7c957dc98055460515f119b
Shuchita Gupta <shuchi2133@gmail.com>
2024-12-13 12:07:16 +0530

    Validate Email Address

--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,57 @@ public class Main {
     	Scanner scanner = new Scanner(System.in);
 
         User user = new User();
+        //Input First name
+        while(true) {
+        	System.out.print("Enter First Name: ");
+            String inputFirstName = scanner.nextLine();
 
-        System.out.print("Enter First Name: ");
-        String inputFirstName = scanner.nextLine();
+            user.setFirstName(inputFirstName);
 
-        user.setFirstName(inputFirstName);
-
-        boolean isValid = user.validateFirstName();
-        if(isValid) {
+            boolean isValid = user.validateFirstName();
+            if(!isValid) {
+            	System.out.println("Not Valid\n"
+            			+ "First name starts with Cap and has minimum 3 characters\r\n");
+            }
+            else {
+            	break;
+            }    	
+        }
+        //Input last name
+        while(true) {
         	System.out.print("Enter Last Name: ");
             String inputLastName = scanner.nextLine();
 
             user.setLastName(inputLastName);
 
-            isValid = user.validateLastName();
-            if(isValid) {
-            	System.out.print("Valid");
+            boolean isValid = user.validateLastName();
+            if(!isValid) {
+            	System.out.println("Not Valid\n"
+            			+ "Last name starts with Cap and has minimum 3 characters\r\n");
             }
             else {
-            	System.out.println("Not Valid\n"
-        			+ "Last name starts with Cap and has minimum 3 characters\r\n");	
+            	break;
             }
         }
-        else {
-        	System.out.println("Not Valid\n"
-        			+ "First name starts with Cap and has minimum 3 characters\r\n");
-        }
+        //Input email
+        while(true) {
+        	System.out.print("Enter Email: ");
+            String inputEmail = scanner.nextLine();
 
+            user.setEmail(inputEmail);
+
+            boolean isValid = user.validateEmail();
+            if(!isValid) {
+            	System.out.println("Email Not Valid\n");
+            }
+            else {
+            	System.out.print("Valid");
+            	break;
+            }
+        }
         
         scanner.close();
+          
+        
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,6 +1,7 @@
 public class User {
     private String firstName;
     private String lastName;
+    private String email;
 
     public String getFirstName() {
         return firstName;
@@ -8,12 +9,18 @@ public class User {
     public String getLastName() {
         return lastName;
     }
+    public String getEmail() {
+        return email;
+    }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public boolean validateFirstName() {
@@ -23,5 +30,9 @@ public class User {
     public boolean validateLastName() {
         String lastNamePattern = "^[A-Z][a-zA-Z]{2,}$";
         return this.lastName.matches(lastNamePattern);
+    }
+    public boolean validateEmail() {
+        String emailPattern = "^[a-zA-Z0-9_+&*-.]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}$";
+        return this.email.matches(emailPattern);
     }
 }
