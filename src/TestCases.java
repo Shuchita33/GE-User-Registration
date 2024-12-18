877189dbfb59f6b9e608f666be90e558258c9d29
Shuchita Gupta <shuchi2133@gmail.com>
2024-12-18 13:19:37 +0530

    Added testcases

---
+++ src/TestCases.java
@@ -0,0 +1,76 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCases {
+    User User=new User();
+    @Test
+    public void test1() {
+        assertTrue(User.validateFirstName("asjba"));
+    }
+
+    @Test
+    public void test2() {
+        assertFalse(User.validateFirstName("Jo"));
+    }
+
+    @Test
+    public void test3() {
+        assertFalse(User.validateFirstName("SHB"));
+    }
+
+    @Test
+    public void test4() {
+        assertTrue(User.validateLastName("Ddnse"));
+    }
+
+    @Test
+    public void test5() {
+        assertFalse(User.validateLastName("D"));
+    }
+
+    @Test
+    public void test6() {
+        assertTrue(User.validateEmail("jR@hd.com"));
+    }
+
+    @Test
+    public void test7() {
+        assertTrue(User.validateEmail("2gsvd.jd.com"));
+    }
+
+    @Test
+    public void test8() {
+        assertFalse(User.validateEmail("4sb,sjnc.co.in"));
+    }
+
+    @Test
+    public void test9() {
+        assertTrue(User.validateMobileNumber("91 9919819801"));
+    }
+
+    @Test
+    public void test10() {
+    	assertTrue(User.validateMobileNumber("919919819801"));
+    }
+
+    @Test
+    public void test11() {
+    	assertTrue(User.validatePassword("Abc@1234"));
+    }
+
+    @Test
+    public void test12() {
+        assertFalse(User.validatePassword("Ab@1"));
+    }
+
+  
+    @Test
+    public void test13() {
+        assertFalse(User.validatePassword("Abc12345"));
+    }
+
+    @Test
+    public void test14() {
+        assertFalse(User.validatePassword("Abc@@1234"));
+    }
+}
