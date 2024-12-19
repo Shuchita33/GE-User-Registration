47d04eaa236f25939f8dd272f282936b2da6e869
Shuchita Gupta <shuchi2133@gmail.com>
2024-12-19 23:02:29 +0530

    Refactored code to Throw Exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,87 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
         //System.out.println("Welcome to User Registration System which ensures all validations are in place during a User Entry.");
     	Scanner scanner = new Scanner(System.in);
 
         User user = new User();
         //Input First name
-//        while(true) {
-//        	System.out.print("Enter First Name: ");
-//            String inputFirstName = scanner.nextLine();
-//
-//            boolean isValid = user.validateFirstName(inputFirstName);
-//            if(!isValid) {
-//            	System.out.println("Not Valid\n"
-//            			+ "First name starts with Cap and has minimum 3 characters\r\n");
-//            }
-//            else {//set the first name once it is validated
-//            	user.setFirstName(inputFirstName);
-//            	break;
-//            }    	
-//        }
-//        //Input last name
-//        while(true) {
-//        	System.out.print("Enter Last Name: ");
-//            String inputLastName = scanner.nextLine();
-//
-//            boolean isValid = user.validateLastName(inputLastName);
-//            if(!isValid) {
-//            	System.out.println("Not Valid\n"
-//            			+ "Last name starts with Cap and has minimum 3 characters\r\n");
-//            }
-//            else {
-//            	user.setLastName(inputLastName);
-//            	break;
-//            }
-//        }
-//        //Input email
-//        while(true) {
-//        	System.out.print("Enter Email: ");
-//            String inputEmail = scanner.nextLine();
-//
-//            boolean isValid = user.validateEmail(inputEmail);
-//            if(!isValid) {
-//            	System.out.println("Email Not Valid\n");
-//            }
-//            else {//set the email once it is validated
-//            	user.setEmail(inputEmail);
-//            	break;
-//            }
-//        }
-//        //Input Mobile
-//        while (true) {
-//            System.out.print("Enter Mobile Number: ");
-//            String phone = scanner.nextLine();
-//
-//            boolean isValid = user.validateMobileNumber(phone);
-//
-//            if (isValid) {
-//            	//System.out.print("Valid");
-//            	user.setPhone(phone);
-//                break; 
-//            } else {
-//                System.out.println("Invalid Mobile Number.\nCountry code follow by space and 10 digit number");
-//            }
-//        }
-//        // Input Password RULE 1,RULE 2,RULE 3, RULE 4
-//        while (true) {
-//            System.out.print("Enter Password : ");
-//            String inputPassword = scanner.nextLine();
-//            boolean isValid = user.validatePassword(inputPassword);
-//
-//            if (isValid) {
-//            	System.out.print("Valid");
-//            	user.setPassword(inputPassword);
-//                break; 
-//            } else {
-//                System.out.println("Invalid Password. Must be at least 8 characters long, "
-//                		+ "at least 1 Upper Case, atleast a numeric char and a special character");
-//            }
-//        }
+        while(true) {
+        	System.out.print("Enter First Name: ");
+            String inputFirstName = scanner.nextLine();
+
+            try {
+            	user.validateFirstName(inputFirstName);
+            	user.setFirstName(inputFirstName);
+            	break;
+
+            }catch(UserException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        //Input last name
+        while(true) {
+        	System.out.print("Enter Last Name: ");
+            String inputLastName = scanner.nextLine();
+
+            try{
+            	user.validateLastName(inputLastName);
+            	user.setLastName(inputLastName);
+            	break;
+            }catch(UserException e) {
+                System.out.println(e.getMessage());
+            }
+         }
+        //Input email
+        while(true) {
+        	System.out.print("Enter Email: ");
+            String inputEmail = scanner.nextLine();
+
+            try{
+            	user.validateEmail(inputEmail);
+            	user.setEmail(inputEmail);
+            	break;
+            }catch(UserException e) {
+                System.out.println(e.getMessage());;
+            }
+
+        }
+        //Input Mobile
+        while (true) {
+            System.out.print("Enter Mobile Number: ");
+            String phone = scanner.nextLine();
+
+            try{
+            	user.validateMobileNumber(phone);
+            	user.setPhone(phone);
+            	break;
+            }catch(UserException e) {
+                System.out.println(e.getMessage());;
+            }
+
+        }
+        // Input Password RULE 1,RULE 2,RULE 3, RULE 4
+        while (true) {
+            System.out.print("Enter Password : ");
+            String inputPassword = scanner.nextLine();
+            try{
+            	user.validatePassword(inputPassword);
+            	user.setPassword(inputPassword);
+            	break;
+            }catch(UserException e) {
+                System.out.println(e.getMessage());;
+            }
+        }
+
+    //EMAIL SAMPLES
         ArrayList<String> inputs = new ArrayList<>();
         while(true) {
         	System.out.print("Provide email samples: ");
             String inputEmail = scanner.nextLine();
             System.out.print("Enter n to exit: ");
-           
+
             if(inputEmail.contentEquals("n")){
             	break;
             }
@@ -94,9 +89,9 @@ public class Main {
         }
         String[] emails=inputs.toArray(new String[0]);
 		user.validateMultipleEmails(emails);
-        
+
         scanner.close();
-          
-        
     }
+
 }
+
--- src/User.java
+++ src/User.java
@@ -37,39 +37,52 @@ public class User {
         this.password = password;
     }
 
-    public boolean validateFirstName(String firstName) {
+    public void validateFirstName(String firstName) throws UserException {
         String firstNamePattern = "^[A-Z][a-zA-Z]{2,}$";
-        return firstName.matches(firstNamePattern);
+        if(!firstName.matches(firstNamePattern)){
+        	throw new UserException("Not Valid,First name starts with Cap and has minimum 3 characters");
+        }
     }
-    public boolean validateLastName(String lastName) {
+    public void validateLastName(String lastName) throws UserException {
         String lastNamePattern = "^[A-Z][a-zA-Z]{2,}$";
-        return lastName.matches(lastNamePattern);
+        if(!lastName.matches(lastNamePattern)) {
+        	throw new UserException("Not Valid,Last name starts with Cap and has minimum 3 characters");
+        };
     }
-    public boolean validateEmail(String email) {
+    public void validateEmail(String email) throws UserException {
         String emailPattern = "^[a-zA-Z0-9_+&*-.]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}$";
-        return email.matches(emailPattern);
+        if(!email.matches(emailPattern)) {
+        	throw new UserException("Email Not Valid,");
+        };
     }
-    public boolean validateMobileNumber(String phone) {
+    public void validateMobileNumber(String phone) throws UserException {
         String numberPattern = "^[0-9]{2} [0-9]{10}$";
-        return phone.matches(numberPattern);
+        if(!phone.matches(numberPattern)) {
+        	throw new UserException("Invalid Mobile Number. Country code follow by space and 10 digit number");
+        };
     }
 //    public boolean validatePassword(String password) {
 //        return password.length() >= 8;
 //    }
-	public boolean validatePassword(String password) {
+	public void validatePassword(String password) throws UserException {
 //      String passwordPattern = "^(?=.*[A-Z]).{8,}$";
 //		String passwordPattern = "^(?=.*[A-Z])(?=.*\\d).{8,}$";
 		String passwordPattern = "^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$";
-	  return password.matches(passwordPattern);
+	  if(!password.matches(passwordPattern)) {
+		  throw new UserException("Invalid Password. Must be at least 8 characters long, at least 1 Upper Case, atleast a numeric char and a special character");
+	  };
 	}
 	public void validateMultipleEmails(String[] emails) {
 	    System.out.println("Validating Email Samples:");
 	    for (String email : emails) {
-	        if (validateEmail(email)) {
-	            System.out.println(email + " -- Valid");
-	        } else {
-	            System.out.println(email + " -- Invalid");
-	        }
+	    	try {
+	    		validateEmail(email);
+		        System.out.println(email + " -- Valid");
+	    	}
+	    	catch(UserException e){
+                System.out.println(email + " -- "+ e.getMessage());
+	    	}
+	        
 	    }
 	}
      
